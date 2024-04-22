f1622720b8edc379c969ba5886b701a9205d5895
Gor Arutyunyan <Rid03@yandex.ru>
2024-04-22 18:01:31 +0300

    Trying useless autogame

---
+++ src/main/java/org/example/javaBook/GameLauncher.java
@@ -0,0 +1,8 @@
+package org.example.javaBook;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
